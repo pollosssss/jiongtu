55272396ba53cef594fee1545698d64e3f1e0635
pollosss <jiyangmax@163.com>
2023-10-18 16:27:11 +0800

    remove comments

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ url = 'https://www.gamersky.com/ent/147/'
 pool = ThreadPoolExecutor(max_workers=10)
 root_path = './images/'
 path = ""
-# 发送HTTP请求并获取页面内容
+
 def fetch_pics():
     if not os.path.exists(root_path):
         os.mkdir(root_path)
@@ -27,7 +27,7 @@ def fetch_pics():
                 os.mkdir(path)
             else:
                 return None
-            first_a = target_ul.find('a')  # 替换为你要查找的class
+            first_a = target_ul.find('a')
             if first_a:
                 first_a_url = first_a.get('href')
                 images_pagination(first_a_url)
@@ -45,21 +45,17 @@ def store_images(url):
     if image_url_response.status_code == 200:
         image_soup = BeautifulSoup(image_url_response.text, 'html.parser')
 
-        # 查找页面上的所有图片标签
         img_tags = image_soup.find_all('img', class_="picact")
 
-        # 遍历每个图片标签，下载图片并保存到本地
         for img_tag in img_tags:
-            img_url = img_tag.get('src')  # 获取图片的URL
+            img_url = img_tag.get('src')
             if img_url and not img_url.endswith(".gif"):
-                img_url = urljoin(url, img_url)  # 构建完整的图片URL
+                img_url = urljoin(url, img_url)
                 img_response = requests.get(img_url)
                 if img_response.status_code == 200:
-                    # 从URL中提取图片文件名
                     global path
                     img_filename = os.path.join(path, os.path.basename(img_url))
 
-                    # 保存图片到本地
                     with open(img_filename, 'wb') as img_file:
                         img_file.write(img_response.content)
                     print(f'Saved: {img_filename}')
